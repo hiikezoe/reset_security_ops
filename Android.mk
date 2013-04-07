3eba4956042a523c14baad90accf994f3bc4d738
Hiroyuki Ikezoe <hiikezoe@gnome.org>
2013-04-07 12:00:52 +0900

    Initial import

---
+++ Android.mk
@@ -0,0 +1,17 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES :=  \
+  main.c            \
+  shlcdc_mmap.c
+
+LOCAL_MODULE := reset_security_ops
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_STATIC_LIBRARIES := libdiagexploit
+
+include $(BUILD_EXECUTABLE)
+
+include $(call all-makefiles-under,$(LOCAL_PATH))
