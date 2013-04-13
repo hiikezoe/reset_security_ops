6dee38609a1a65c274049d328f9a92bda2915fc5
Hiroyuki Ikezoe <hiikezoe@gnome.org>
2013-04-13 12:07:04 +0900

    Reformat

--- Android.mk
+++ Android.mk
@@ -2,17 +2,11 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES :=  \
-  main.c
-
+LOCAL_SRC_FILES := main.c
 LOCAL_MODULE := reset_security_ops
-
 LOCAL_MODULE_TAGS := optional
-
 LOCAL_LDFLAGS := --static
-
 LOCAL_FORCE_STATIC_EXECUTABLE := true
-
 LOCAL_STATIC_LIBRARIES += libcutils libc
 
 include $(BUILD_EXECUTABLE)
